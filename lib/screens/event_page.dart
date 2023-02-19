30b522ac81b4b6fd49572ace4eb18e4f49c48251
karmakatoro <karmakatoro@gmail.com>
2023-02-19 10:13:08 +0200

    creating list view builder for plannings

--- lib/screens/event_page.dart
+++ lib/screens/event_page.dart
@@ -1,7 +1,27 @@
 import 'package:flutter/material.dart';
 
-class EventPage extends StatelessWidget {
+class EventPage extends StatefulWidget {
   const EventPage({Key? key}) : super(key: key);
+
+  @override
+  _EventPageState createState() => _EventPageState();
+}
+
+class _EventPageState extends State<EventPage> {
+  final events = [
+    {
+      "speaker": "Trez Kit",
+      "date": "17h00-18h00",
+      "subject": "La programmation web et mobile",
+      "avatar": "c2"
+    },
+    {
+      "speaker": "Dorcas Promesse",
+      "date": "17h30-18h00",
+      "subject": "Au coeur de la programmation C#",
+      "avatar": "c3"
+    }
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -10,7 +30,24 @@ class EventPage extends StatelessWidget {
         title: Text("Planning du salon"),
       ),
       body: Center(
-        child: Text("Prochainement disponible"),
+        child: ListView.builder(
+          itemCount: events.length,
+          itemBuilder: (context, index) {
+            final event = events[index];
+            final avatar = event['avatar'];
+            final speaker = event['speaker'];
+            final subject = event['subject'];
+            final date = event['date'];
+            return Card(
+              child: ListTile(
+                leading: Image.asset("assets/images/$avatar.jpg"),
+                title: Text("$speaker - $date"),
+                subtitle: Text("$subject"),
+                trailing: Icon(Icons.info),
+              ),
+            );
+          },
+        ),
       ),
     );
   }
