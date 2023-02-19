82da35e4b186d43f9cc97df39274c21d7bd217dc
karmakatoro <karmakatoro@gmail.com>
2023-02-19 09:29:54 +0200

    creating dart files for screens

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:test_app/screens/home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,62 +11,5 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(home: HomePage());
-  }
-}
-
-class EventPage extends StatelessWidget {
-  const EventPage({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Planning du salon"),
-      ),
-      body: Center(
-        child: Text("Prochainement disponible"),
-      ),
-    );
-  }
-}
-
-class HomePage extends StatelessWidget {
-  const HomePage({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Aura Test App"),
-      ),
-      body: Center(
-          child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Image.asset(
-            "assets/images/logo-dark.png",
-            width: 60,
-            height: 60,
-          ),
-          const Text("Aura Test App 2023",
-              style: TextStyle(fontSize: 24, fontFamily: "Inter"),
-              textAlign: TextAlign.center),
-          const Text(
-            "Salon virtuel de l'informatique du 27 mars au 3 avril 2023",
-            style: TextStyle(fontSize: 18),
-            textAlign: TextAlign.center,
-          ),
-          Padding(padding: EdgeInsets.all(10)),
-          ElevatedButton(
-            style: ButtonStyle(
-                backgroundColor: MaterialStatePropertyAll(Colors.green)),
-            onPressed: () => print("Coucou"),
-            child: Text("Planning", style: TextStyle(fontSize: 16)),
-          ),
-        ],
-      )),
-    );
   }
 }
---
+++ lib/screens/home_page.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:test_app/screens/event_page.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Aura Test App"),
+      ),
+      body: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset(
+            "assets/images/logo-dark.png",
+            width: 60,
+            height: 60,
+          ),
+          const Text("Aura Test App 2023",
+              style: TextStyle(fontSize: 24, fontFamily: "Inter"),
+              textAlign: TextAlign.center),
+          const Text(
+            "Salon virtuel de l'informatique du 27 mars au 3 avril 2023",
+            style: TextStyle(fontSize: 18),
+            textAlign: TextAlign.center,
+          ),
+          Padding(padding: EdgeInsets.only(top: 10)),
+          ElevatedButton.icon(
+            style: ButtonStyle(
+                padding: MaterialStatePropertyAll(EdgeInsets.all(15)),
+                backgroundColor: MaterialStatePropertyAll(Colors.green)),
+            onPressed: () {
+              Navigator.push(context,
+                  PageRouteBuilder(pageBuilder: (_, __, ___) => EventPage()));
+            },
+            label: Text("Planning", style: TextStyle(fontSize: 16)),
+            icon: Icon(Icons.calendar_month),
+          ),
+        ],
+      )),
+    );
+  }
+}
