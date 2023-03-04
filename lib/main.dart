bc2d6e6c3778dca855c3fe7d03a0675d1672da5f
karmakatoro <karmakatoro@gmail.com>
2023-03-04 08:38:45 +0200

    set main as stf widget

--- lib/main.dart
+++ lib/main.dart
@@ -5,9 +5,14 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
 
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
