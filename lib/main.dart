8c2bccdc1f423331819e67b92d3d0416e5e4884d
karmakatoro <karmakatoro@gmail.com>
2023-03-04 08:59:51 +0200

    updates

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,14 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  int _currentIndex = 0;
+
+  setCurrentIndex(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -20,6 +28,8 @@ class _MyAppState extends State<MyApp> {
         appBar: AppBar(title: Text("Aura test_app")),
         body: const HomePage(),
         bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _currentIndex,
+          onTap: (index) => setCurrentIndex(index),
           selectedItemColor: Colors.green,
           unselectedItemColor: Colors.grey,
           iconSize: 32,
