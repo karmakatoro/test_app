f06456da313d6e5c8853070d15f0518b084d0a8a
karmakatoro <karmakatoro@gmail.com>
2023-03-09 10:44:52 +0200

    forms

--- lib/screens/add_event_page.dart
+++ lib/screens/add_event_page.dart
@@ -1,3 +1,30 @@
 import 'package:flutter/material.dart';
 
-st
+class AddEventPage extends StatefulWidget {
+  const AddEventPage({Key? key}) : super(key: key);
+
+  @override
+  _AddEventPageState createState() => _AddEventPageState();
+}
+
+class _AddEventPageState extends State<AddEventPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(20),
+      child: Form(
+        child: Column(
+          children: [
+            TextFormField(
+              decoration: InputDecoration(
+                  labelText: 'Nom de la conference',
+                  hintText: 'Entrez le nom de la conferance',
+                  border: OutlineInputBorder()),
+            ),
+            ElevatedButton(onPressed: () {}, child: Text("Envoyer"))
+          ],
+        ),
+      ),
+    );
+  }
+}
