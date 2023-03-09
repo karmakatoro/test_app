aabb3086181d2cff6a9dd09bc39ba26e311b90a7
karmakatoro <karmakatoro@gmail.com>
2023-03-09 11:33:57 +0200

    upadtes

--- lib/screens/add_event_page.dart
+++ lib/screens/add_event_page.dart
@@ -9,6 +9,16 @@ class AddEventPage extends StatefulWidget {
 
 class _AddEventPageState extends State<AddEventPage> {
   final _formKey = GlobalKey<FormState>();
+  final nameConfController = TextEditingController();
+  final speakerNameController = TextEditingController();
+
+  @override
+  void dispose() {
+    super.dispose();
+    nameConfController.dispose();
+    speakerNameController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -20,9 +30,24 @@ class _AddEventPageState extends State<AddEventPage> {
             Container(
               margin: EdgeInsets.only(bottom: 10),
               child: TextFormField(
+                  decoration: const InputDecoration(
+                      labelText: 'Nom de la conference',
+                      hintText: 'Entrez le nom de la conferance',
+                      border: OutlineInputBorder()),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return "Champ requis";
+                    }
+                    return null;
+                  },
+                  controller: nameConfController),
+            ),
+            Container(
+              margin: EdgeInsets.only(bottom: 10),
+              child: TextFormField(
                 decoration: const InputDecoration(
-                    labelText: 'Nom de la conference',
-                    hintText: 'Entrez le nom de la conferance',
+                    labelText: 'Nom du speaker',
+                    hintText: 'Entrez le nom ddu speaker',
                     border: OutlineInputBorder()),
                 validator: (value) {
                   if (value == null || value.isEmpty) {
@@ -30,13 +55,23 @@ class _AddEventPageState extends State<AddEventPage> {
                   }
                   return null;
                 },
+                controller: speakerNameController,
               ),
             ),
             SizedBox(
               width: double.infinity,
               height: 50,
               child: ElevatedButton(
-                  onPressed: () {}, child: const Text("Envoyer")),
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      final confName = nameConfController.text;
+                      final speakerName = speakerNameController.text;
+                      ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(content: Text("Envoi en cours")));
+                      FocusScope.of(context).requestFocus(FocusNode());
+                    }
+                  },
+                  child: const Text("Envoyer")),
             ),
           ],
         ),
