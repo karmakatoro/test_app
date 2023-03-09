65056e16fd0e165d0ff3934eaa529562a85c591e
karmakatoro <karmakatoro@gmail.com>
2023-03-09 10:24:00 +0200

    restart

--- lib/screens/add_event_page.dart
+++ lib/screens/add_event_page.dart
@@ -1,12 +1,3 @@
 import 'package:flutter/material.dart';
 
-class AddEventPage extends StatelessWidget {
-  const AddEventPage({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Text("Formulaire"),
-    );
-  }
-}
+st
